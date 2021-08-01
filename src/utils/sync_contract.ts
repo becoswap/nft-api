e8c9bf002233b8529568c3dcc90024911b0c7602
becoswap-dev <cool@becoswap.com>
2021-08-01 14:44:00 +0700

    fix sync contract

--- src/utils/sync_contract.ts
+++ src/utils/sync_contract.ts
@@ -9,10 +9,10 @@ const SYNC_MAX_BLOCK = 1000;
 export const syncContract = async (contractAddress: string, _startBlock: number, handler) => {
   const syncStatuses = await SyncStatus.findOrCreate({
     where: { id: contractAddress },
-    defaults: { lastBlock: _startBlock },
+    defaults: { lastBlock: _startBlock - 1 },
   });
   const syncStatus: any = syncStatuses[0];
-  let startBlock = syncStatus.lastBlock;
+  let startBlock = syncStatus.lastBlock + 1;
   let lastestBlock = await kaiWeb3.getBlockNumber();
   while (true) {
     const t = await database.transaction();
@@ -21,16 +21,21 @@ export const syncContract = async (contractAddress: string, _startBlock: number,
       if (lastestBlock - startBlock > SYNC_MAX_BLOCK) {
         endBlock += SYNC_MAX_BLOCK;
       } else {
-        endBlock = lastestBlock;
-        lastestBlock = await kaiWeb3.getBlockNumber();
-        await sleep(5000);
+        while (true) {
+          lastestBlock = await kaiWeb3.getBlockNumber();
+          if (endBlock > lastestBlock) {
+            sleep(5000);
+          } else {
+            endBlock = lastestBlock;
+            break;
+          }
+        }
       }
-
       await handler(t, startBlock, endBlock);
       syncStatus.set('lastBlock', endBlock);
       await syncStatus.save({ transaction: t });
       await t.commit();
-      startBlock = endBlock;
+      startBlock = endBlock + 1;
     } catch (err) {
       console.log('sync contract ' + contractAddress + ' err: ', err);
       await t.rollback();
