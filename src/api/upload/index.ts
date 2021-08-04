db190ea429eea6367e4d79267708d094d6db5f58
becoswap-dev <cool@becoswap.com>
2021-08-04 19:08:28 +0700

    update upload signature

--- src/api/upload/index.ts
+++ src/api/upload/index.ts
@@ -13,16 +13,20 @@ const signature = ctx => {
 
   const id = crypto.randomBytes(16).toString('hex');
   const key = 'static/ntfs/' + id + '.' + body.suffix;
-  const url = s3.getSignedUrl('putObject', {
+  const url = s3.createPresignedPost({
     Bucket: 'becoswap',
-    Key: key,
-    Expires: 60
+    Expires: 300,
+    Conditions: [['content-length-range', 0, 104857600]],
+
+    Fields: {
+      key: key,
+    },
   });
 
   ctx.status = 200;
   ctx.body = {
-    uploadUrl: url,
-    fileUrl: "https://d3g5bzmawnfi61.cloudfront.net/" + key
+    ...url,
+    fileUrl: 'https://d3g5bzmawnfi61.cloudfront.net/' + key,
   };
 };
 
