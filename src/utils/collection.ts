6882abc01540298a9f7d73d6459419c362b7590e
becoswap-dev <cool@becoswap.com>
2021-09-20 10:37:53 +0700

    fix

--- src/utils/collection.ts
+++ src/utils/collection.ts
@@ -1,11 +1,15 @@
+import { QueryTypes } from 'sequelize';
 import sequelize from '../database';
 import database from '../database';
 const NFT = database.models.nft;
 const Collection = database.models.collection;
 
 export const saveTotalOwner = async (collectionId: number) => {
-  const rows = await sequelize.query(`select count(DISTINCT owner) from nfts;`);
+  const rows = await sequelize.query(`select count(DISTINCT owner) from nfts where "nftType"=?;`, {
+    replacements: [collectionId],
+    type: QueryTypes.SELECT,
+  });
   const col = await Collection.findByPk(collectionId);
-  col.totalOwner = rows[0][0].count;
+  col.totalOwner = rows[0].count;
   await col.save();
 };
