89255f7cc75c8de2ac2d256d20ee2546a528015a
becoswap-dev <cool@becoswap.com>
2021-09-26 17:54:47 +0700

    UPDATE TIMEOUT

--- src/utils/web3.ts
+++ src/utils/web3.ts
@@ -1,9 +1,9 @@
-import { ethers } from "ethers";
-import { KAI_RPC } from "../constants"
+import { ethers } from 'ethers';
+import { KAI_RPC } from '../constants';
 
-const kaiWeb3 = new ethers.providers.JsonRpcProvider(KAI_RPC);
+const kaiWeb3 = new ethers.providers.JsonRpcProvider({
+  url: KAI_RPC,
+  timeout: 5000,
+});
 
-
-export {
-    kaiWeb3
-}
+export { kaiWeb3 };
