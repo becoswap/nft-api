c1346798853d27b7d16e2d318f8fafc0dbcc5d26
becoswap-dev <cool@becoswap.com>
2021-09-26 15:18:20 +0700

    update

--- src/utils/sync_contract.ts
+++ src/utils/sync_contract.ts
@@ -1,54 +1,11 @@
 import { kaiWeb3 } from './web3';
 import database from '../database';
 import { sleep } from './sleep';
+import { getLatestBlock } from './blockNumber';
 
 const SyncStatus = database.models.sync_status;
 
 const SYNC_MAX_BLOCK = 1000;
-
-export const syncContract = async (contractAddress: string, _startBlock: number, handler) => {
-  const syncStatuses = await SyncStatus.findOrCreate({
-    where: { id: contractAddress },
-    defaults: { lastBlock: _startBlock - 1 },
-  });
-  const syncStatus: any = syncStatuses[0];
-  let startBlock = syncStatus.lastBlock + 1;
-  let lastestBlock = 0;
-  while (true) {
-    try {
-      lastestBlock = (await kaiWeb3.getBlockNumber()) - 1;
-      if (lastestBlock < startBlock) {
-        await sleep(5000);
-        continue;
-      }
-    } catch (err) {
-      const d = new Date();
-      console.log(d.toString(), ': sync contract ' + contractAddress + ' err: ', err.message);
-      await sleep(10000);
-      continue;
-    }
-
-    const t = await database.transaction();
-    try {
-      let endBlock = startBlock;
-      if (lastestBlock - startBlock > SYNC_MAX_BLOCK) {
-        endBlock += SYNC_MAX_BLOCK;
-      } else {
-        endBlock = lastestBlock;
-      }
-      await handler(t, startBlock, endBlock);
-      syncStatus.set('lastBlock', endBlock);
-      await syncStatus.save({ transaction: t });
-      await t.commit();
-      startBlock = endBlock + 1;
-    } catch (err) {
-      await t.rollback();
-      const d = new Date();
-      console.error(d.toString(), ': sync contract ' + contractAddress + ' err: ', err.message);
-      await sleep(20000);
-    }
-  }
-};
 
 export const syncContractv2 = async (contractAddress: string, _startBlock: number, handler) => {
   const syncStatuses = await SyncStatus.findOrCreate({
@@ -60,7 +17,7 @@ export const syncContractv2 = async (contractAddress: string, _startBlock: numbe
   let lastestBlock = 0;
   while (true) {
     try {
-      lastestBlock = (await kaiWeb3.getBlockNumber()) - 1;
+      lastestBlock = getLatestBlock();
       if (lastestBlock < startBlock) {
         await sleep(5000);
         continue;
