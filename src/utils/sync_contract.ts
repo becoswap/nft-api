e7ee8a8edf4893ffb7da74bcca7a2f1102c302de
becoswap-dev <cool@becoswap.com>
2021-12-18 17:11:37 +0700

    update

--- src/utils/sync_contract.ts
+++ src/utils/sync_contract.ts
@@ -5,7 +5,7 @@ import { getLatestBlock } from './blockNumber';
 
 const SyncStatus = database.models.sync_status;
 
-const SYNC_MAX_BLOCK = 200;
+const SYNC_MAX_BLOCK = 100;
 
 export const syncContractv2 = async (
   contractAddress: string,
