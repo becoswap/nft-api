c130e82d83c0b285590381ca85b94b27039d9d0e
becoswap-dev <cool@becoswap.com>
2021-12-21 19:37:48 +0700

    update

--- src/indexer.ts
+++ src/indexer.ts
@@ -8,9 +8,10 @@ Sequelize.useCLS(namespace);
 import YAML from 'yaml';
 
 import sequelize from './database';
-import { syncContractv2 } from './utils/sync_contract';
-import { kaiWeb3 } from './utils/web3';
-import { syncLatestBlock } from './utils/blockNumber';
+import { kaiWeb3, web3 } from './utils/web3';
+import { getLatestBlock, syncLatestBlock } from './utils/blockNumber';
+import database from './database';
+import { sleep } from './utils/sleep';
 
 const getDirectories = source =>
   readdirSync(source, { withFileTypes: true })
@@ -32,53 +33,33 @@ async function assertDatabaseConnectionOk() {
 }
 
 function getAbi(module, dataSource) {
-  return fs.readFileSync(
-    `./src/mods/${module}/` + dataSource.source.abi.replace('./', ''),
-    'utf8'
-  );
+  return fs.readFileSync(`./src/mods/${module}/` + dataSource.source.abi.replace('./', ''), 'utf8');
 }
 
-function startJob(module, dataSource) {
-  const abi = fs.readFileSync(
-    `./src/mods/${module}/` + dataSource.source.abi.replace('./', ''),
-    'utf8'
-  );
-  const contract = new ethers.Contract(dataSource.source.address, abi, kaiWeb3);
-  const handlers = require(`./mods/${module}/` + dataSource.mapping.file.replace('./', ''));
-  const eventHandlersMaps = {};
-  for (var handler of dataSource.mapping.eventHandlers) {
-    eventHandlersMaps[handler.event] = handler.handler;
-  }
-  syncContractv2(dataSource.source.address, dataSource.source.startBlock, async (start, end) => {
-    const events = await contract.queryFilter({}, start, end);
-    for (var event of events) {
-      const hanlderFn = handlers[eventHandlersMaps[event.event]];
-
-      if (hanlderFn) {
-        await handlers[eventHandlersMaps[event.event]](event);
-      }
-    }
-  });
-}
-
+const SyncStatus = database.models.sync_status;
+const SYNC_MAX_BLOCK = 100;
 async function start() {
   await assertDatabaseConnectionOk();
   await syncLatestBlock();
 
   let handlers = {};
   let firstBlock = 0;
+  let topics = [];
+  let addreses = [];
   for (var module of modules) {
     const file = fs.readFileSync(`./src/mods/${module}/source.yaml`, 'utf8');
     const sourceConfig = YAML.parse(file);
     for (const dataSource of sourceConfig.dataSources) {
       const abi = getAbi(module, dataSource);
-      handlers[dataSource.source.address] = {}
+      handlers[dataSource.source.address] = {};
       firstBlock = dataSource.source.startBlock;
+      addreses.push(dataSource.source.address);
       let mapping = require(`./mods/${module}/` + dataSource.mapping.file.replace('./', ''));
-      for(let h of dataSource.mapping.eventHandlers) {
+      for (let h of dataSource.mapping.eventHandlers) {
         let iface = new ethers.utils.Interface(abi);
         let topic = iface.getEventTopic(h.event);
-        handlers[dataSource.source.address][topic] = (log) => {
+        topics.push(topic);
+        handlers[dataSource.source.address][topic] = log => {
           const args = iface.decodeEventLog(h.event, log.data, log.topics);
           const e = {
             args,
@@ -87,28 +68,61 @@ async function start() {
             address: log.address,
             event: h.event,
             transactionHash: log.transactionHash,
-          }
+            getTransaction: () => {
+              return kaiWeb3.getTransaction(log.transactionHash);
+            },
+          };
           return mapping[h.handler](e);
-        }
-      } 
+        };
+      }
     }
   }
 
-
-  //
- while (true) {
-  const logs = await kaiWeb3.getLogs({
-    fromBlock: firstBlock,
-    toBlock: firstBlock + 10,
+  const syncStatuses = await SyncStatus.findOrCreate({
+    where: { id: 'xx-1' },
+    defaults: { lastBlock: firstBlock },
   });
-  firstBlock += 10;
-  console.log(firstBlock, logs.length)
-  for (var log of logs) {
-    if (handlers[log.address] && handlers[log.address][log.topics[0]]) {
-      handlers[log.address][log.topics[0]](log);
+  const syncStatus: any = syncStatuses[0];
+  let startBlock = syncStatus.lastBlock;
+  let lastestBlock = 0;
+
+  while (true) {
+    try {
+      lastestBlock = getLatestBlock() - 1;
+      if (lastestBlock < startBlock) {
+        await sleep(5000);
+        continue;
+      }
+
+      let endBlock = startBlock;
+      if (lastestBlock - startBlock > SYNC_MAX_BLOCK) {
+        endBlock += SYNC_MAX_BLOCK;
+      } else {
+        endBlock = lastestBlock;
+      }
+
+      let logs = await web3.eth.getPastLogs({
+        fromBlock: startBlock,
+        toBlock: endBlock,
+        topics: [topics],
+        address: addreses,
+      });
+
+      logs = logs.filter(log => handlers[log.address] && handlers[log.address][log.topics[0]]);
+      await database.transaction(async () => {
+        for (let log of logs) {
+          await handlers[log.address][log.topics[0]](log);
+        }
+        startBlock = endBlock + 1;
+        syncStatus.set('lastBlock', startBlock);
+        await syncStatus.save();
+      });
+    } catch (e) {
+      const d = new Date();
+      console.error(d.toString(), ': sync err: ', e.message);
+      await sleep(20000);
     }
   }
- }
 }
 
 start();
