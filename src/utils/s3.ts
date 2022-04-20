6f7ddbfbc5ade0fc021db0c8277b1473f4640f04
becoswap-dev <83570067+becoswap-dev@users.noreply.github.com>
2022-04-20 12:42:51 +0700

    Update s3.ts

--- src/utils/s3.ts
+++ src/utils/s3.ts
@@ -1,8 +1,8 @@
 import AWS from "aws-sdk"
 
 const s3 = new AWS.S3({
-  accessKeyId: 'AKIAWCKLJSTF4WWBH7W5',
-  secretAccessKey: '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',
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   region: 'us-east-2'
 });
 
